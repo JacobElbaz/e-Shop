00e5fb9510e852cda74c22021d33bb0ab8baa09b
Elie Bracha <eliebrachapro@gmail.com>
2022-03-11 23:21:57 +0200

    error in arrays

--- tests/Arrays.spec.js
+++ tests/Arrays.spec.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 describe('Array', () => {
   describe('sort', () => {
     it('should sort the array by name', () => {
-      var name = ['Dani', 'Moshe', 'Adam'];
+      var names = ['Dani', 'Moshe', 'Adam'];
       expect(names.sort()).to.be.eql(['Adam', 'Dani', 'Moshe']);
     });
   });
