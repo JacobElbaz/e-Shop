bd63ff34e2efdab5cb56a883ce97374a7cd6ee85
Elie Bracha <eliebrachapro@gmail.com>
2022-03-11 23:18:45 +0200

    Second commit

--- tests/Arrays.spec.js
+++ tests/Arrays.spec.js
@@ -1,1 +1,10 @@
- 
+const expect = require('chai').expect;
+
+describe('Array', () => {
+  describe('sort', () => {
+    it('should sort the array by name', () => {
+      var name = ['Dani', 'Moshe', 'Adam'];
+      expect(names.sort()).to.be.eql(['Adam', 'Dani', 'Moshe']);
+    });
+  });
+});
