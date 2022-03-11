195656c1dc8379bf575a662e75b65a6eaf8f59fb
Elie Bracha <eliebrachapro@gmail.com>
2022-03-12 01:11:45 +0200

    kdj

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.get('/', (res, req) => {
-  res.send('<h1>Hey from server main page</h1>');
+app.get('/', (req, res) => {
+  res.send('<h1>Hey from server main page<h1>');
 });
 
 app.listen(port, () => {
