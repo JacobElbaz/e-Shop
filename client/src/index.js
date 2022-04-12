e01e6449a23289c3e89bf229739cc7945cf4ab58
JacobElbaz <jacobel@post.bgu.ac.il>
2022-04-12 18:17:05 +0300

    delete reportWebVitals

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,6 +11,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-
-reportWebVitals();
