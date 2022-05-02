e9fdea500d18672adc6c32f508df6ea71f8111d7
JacobElbaz <jacobel@post.bgu.ac.il>
2022-05-02 20:01:29 +0300

    product list

--- client/src/Components/ProductsList.js
+++ client/src/Components/ProductsList.js
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getProducts } from '../actions/products.action';
-import {isEmpty} from './Utils';
+import { Table, Button, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { isEmpty } from './Utils';
+import {
+    getProducts,
+    deleteProduct,
+} from '../actions/products.action';
 
-export default function ProductsList() {
+const ProductList = () => {
     const [loadProducts, setLoadProducts] = useState(true);
     const dispatch = useDispatch();
     const products = useSelector((state) => state.allProductsReducer);
@@ -13,17 +18,72 @@ export default function ProductsList() {
             dispatch(getProducts());
             setLoadProducts(false);
         }
-    }, [loadProducts, dispatch])
+    }, [loadProducts, dispatch]);
+
+    const onDeleteClick = (id) => {
+    };
+
+    const onCreateClick = () => {
+    };
 
     return (
-        <div>
-            <ul>
-                {!isEmpty(products[0]) && 
-                products.map((product) => {
-                    return <li>{product.name}</li>
-                })
-                }
-            </ul>
-        </div>
-    )
-}
+        <>
+            <Row className='align-items-center'>
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+                <Col className='text-right'>
+                    <Button className='my-3' onClick={onCreateClick}>
+                        <i className='fas fa-plus'></i> Create Product
+                    </Button>
+                </Col>
+            </Row>
+            <Table striped bordered hover responsive className='table-sm'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th>PRICE</th>
+                        <th>GENRE</th>
+                        <th>CATEGORY</th>
+                        <th>RELEASE DATE</th>
+                        <th>ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!isEmpty(products[0]) &&
+                        products.map((product) => {
+                            return (
+                                <tr key={product._id}>
+                                    <td>
+                                        <Link to={`/admin/products/${product._id}/edit`}>{product._id}</Link>
+                                    </td>
+                                    <td>{product.name}</td>
+                                    <td>${product.price}</td>
+                                    <td>{product.genre}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.releaseDate.split('T')[0]}</td>
+                                    <td>
+                                        <Link to={`/admin/products/${product._id}/edit`}>
+                                            <Button variant='light' className='btn-sm'>
+                                                <i className='fas fa-edit'></i>
+                                            </Button>
+                                        </Link>
+                                        <Button
+                                            variant='danger'
+                                            className='btn-sm'
+                                            onClick={() => onDeleteClick(product._id)}>
+                                            <i className='fas fa-trash'></i>
+                                        </Button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+        </>
+    );
+};
+
+export default ProductList;
