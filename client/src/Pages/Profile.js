10c6ac93a5c1d3eecf521056db419d3c123d007d
Elie Bracha <eliebrachapro@gmail.com>
2022-05-23 18:09:56 +0300

    Finish Update Profile

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { update_username, forgot_password } from '../actions/user.action';
 import { Link } from 'react-router-dom';
-import { Formik } from 'formik';
 
 import { Button, Modal, Form } from 'react-bootstrap';
 import FormContainer from '../Components/FormContainer';
-import {confirmPasswordFormValidationSchema} from '../validations'
-
+import { confirmPasswordFormValidationSchema } from '../validations';
 
 const Profile = () => {
   const user = useSelector((state) => state.userReducer);
@@ -19,16 +17,16 @@ const Profile = () => {
   const [password, setPassword] = useState('');
   const [showModalUsername, setShowModalUsername] = useState(false);
   const [showModalPassword, setShowModalPassword] = useState(false);
+  const [showModalError, setShowModalError] = useState(false);
   const [errors, setErrors] = useState('');
+  const [passwordErrors, setPasswordErrors] = useState('');
 
   const validate = (event) => {
-    
     if (password !== event.target.value) {
-      console.log('ELie');
       setErrors('Password not matched');
+    } else {
+      setErrors('');
     }
-    else
-      setErrors('')
   };
 
   const UsernameChangeHandler = (event) => {
@@ -36,10 +34,12 @@ const Profile = () => {
   };
 
   const PasswordChangeHandler = (event) => {
-    if(event.target.value.length < 6)
-      console.log("Require 6 character");
-    else
+    if (event.target.value.length < 6)
+      setPasswordErrors('Password require 6 character at least');
+    else {
+      setPasswordErrors('');
       setPassword(event.target.value);
+    }
   };
 
   const dispatch = useDispatch();
@@ -51,18 +51,39 @@ const Profile = () => {
 
   const onSubmitPassword = (e) => {
     e.preventDefault();
-    dispatch(forgot_password(initialValues.email, password));
-    setShowModalPassword(true);
+    if (errors === '' && passwordErrors === '') {
+      dispatch(forgot_password(initialValues.email, password));
+      setShowModalPassword(true);
+    } else{
+      setShowModalError(true);
+    } 
   };
 
   const handleClose = () => {
     setShowModalPassword(false);
     setShowModalUsername(false);
+    setShowModalError(false);
     window.location = '/profile';
   };
 
   return (
     <>
+      <Modal show={showModalError} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Confirm password don't match with password or password need 6 character</Modal.Body>
+        <Modal.Footer>
+          <Link
+            type="button"
+            className="btn btn-secondary"
+            to={'/profile'}
+            onClick={handleClose}
+          >
+            OK
+          </Link>
+        </Modal.Footer>
+      </Modal>
       <Modal show={showModalUsername} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Success Update Username</Modal.Title>
@@ -124,40 +145,36 @@ const Profile = () => {
             />
           </Form.Group>
         </Form>
-        <Formik
-        validationSchema={confirmPasswordFormValidationSchema}
-        onSubmit={onSubmitPassword}
-        >
-          <Form >
-            <Form.Group controlId="password">
-              <Form.Label>New Password</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                required
-                placeholder='Enter New Password'
-                defaultValue={user.password}
-                onChange={PasswordChangeHandler}
-              />
-              </Form.Group>
-              
-            <Form.Group controlId="confirmPassword">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                name="confirmPassword"
-                required
-                placeholder='Confirm Password'
-                defaultValue={user.password}
-                onChange={validate}
-              />
-              <p className='text-danger'>{errors}</p>
-              <Button variant="primary" size="sm" type="submit">
-                Update Password
-              </Button>
-            </Form.Group>
-          </Form>
-        </Formik>
+        <Form onSubmit={onSubmitPassword}>
+          <Form.Group controlId="password">
+            <Form.Label>New Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              required
+              placeholder="Enter New Password"
+              defaultValue={user.password}
+              onChange={PasswordChangeHandler}
+            />
+          </Form.Group>
+          <p className="text-danger">{passwordErrors}</p>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control
+              type="password"
+              name="confirmPassword"
+              required
+              placeholder="Confirm Password"
+              defaultValue={user.password}
+              onChange={validate}
+            />
+            <p className="text-danger">{errors}</p>
+            <Button variant="primary" size="sm" type="submit">
+              Update Password
+            </Button>
+          </Form.Group>
+        </Form>
       </FormContainer>
     </>
   );
