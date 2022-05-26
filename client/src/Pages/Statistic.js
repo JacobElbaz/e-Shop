c36e9bba9c2b09ada85c3bb7aec7b2c0f32e7a00
JacobElbaz <jacobel@post.bgu.ac.il>
2022-05-26 15:58:48 +0300

    totals

--- client/src/Pages/Statistic.js
+++ client/src/Pages/Statistic.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Table } from 'react-bootstrap';
+import { Button, Col, Row, Table } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import { isEmpty } from '../Components/Utils';
 import { allProducts, getProducts } from '../actions/products.action';
+import { getOrders } from '../actions/orders.action';
 
 function Statistic() {
   const [loadProducts, setLoadProducts] = useState(true);
@@ -17,23 +18,41 @@ function Statistic() {
   const dispatch = useDispatch();
   const allproduct = useSelector((state) => state.productReducer);
   const products = useSelector((state) => state.allProductsReducer);
+  const orders = useSelector((state) => state.ordersReducer);
   useEffect(() => {
     dispatch(getProducts(undefined, filter.category, filter.genre, sort));
     dispatch(allProducts());
-  }, [sort, filter, loadProducts,totalsales, dispatch]);
-  const sumSales = (allproduct) => {
-      let sum = 0;
-      for(let i = 0; i < allproduct.length; i++){
-        sum += allproduct[i].price * allproduct[i].sales;
-      }
-      return sum;
-
+    dispatch(getOrders());
+  }, [sort, filter, loadProducts, totalsales, dispatch]);
+  const sumBenefits = (allproduct) => {
+    let sum = 0;
+    for (let i = 0; i < allproduct.length; i++) {
+      sum += allproduct[i].price * allproduct[i].sales;
+    }
+    return sum;
   }
 
+  const sumOrders = () => {
+    console.log(orders);
+    return orders.length;
+  }
+
+  const sumSales = () => {
+    let sum = 0;
+    for (let i = 0; i < allproduct.length; i++) {
+      sum += allproduct[i].sales;
+    }
+    return sum;
+  } 
+
   return (
-    <div className="home">
-      <h1>Statistics</h1>
-      <h1>Total Sales: ${sumSales(allproduct)}</h1>
+    <div className="m-5">
+      <Row>
+        <h1>Statistics</h1>
+        <Col><h5>Total orders: {sumOrders()}</h5></Col>
+        <Col><h5>Total sales: {sumSales()}</h5></Col>
+        <Col><h5>Total benefits: ${sumBenefits(allproduct)}</h5></Col>
+      </Row>
       Filter{' '}
       <select
         className="form-select w-25 d-inline"
@@ -82,42 +101,38 @@ function Statistic() {
         <option value="newest">Newest by Date</option>
         <option value="oldest">Oldest by Date</option>
       </select>
-      <Button onClick  = {()=> {setTotalSales(true)}} >Total Sales</Button>
-      {totalsales ? (
-        <h1>Total Sales of Company is ${sumSales(allproduct)}</h1>
-      ) : (
-        <Table striped bordered hover responsive className="table-sm">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>NAME</th>
-              <th>PRICE</th>
-              <th>GENRE</th>
-              <th>CATEGORY</th>
-              <th>RELEASE DATE</th>
-              <th>SALES</th>
-            </tr>
-          </thead>
-          <tbody>
-            {!isEmpty(products[0]) &&
-              products.map((product) => {
-                return (
-                  <tr key={product._id}>
-                    <td>
-                      <Link to={`/product/${product._id}`}>{product._id}</Link>
-                    </td>
-                    <td>{product.name}</td>
-                    <td>${product.price}</td>
-                    <td>{product.genre}</td>
-                    <td>{product.category}</td>
-                    <td>{product.releaseDate.split('T')[0]}</td>
-                    <td>{product.sales} </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </Table>
-      )}
+
+      <Table striped bordered hover responsive className="table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>PRICE</th>
+            <th>GENRE</th>
+            <th>CATEGORY</th>
+            <th>RELEASE DATE</th>
+            <th>SALES</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!isEmpty(products[0]) &&
+            products.map((product) => {
+              return (
+                <tr key={product._id}>
+                  <td>
+                    <Link to={`/product/${product._id}`}>{product._id}</Link>
+                  </td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.genre}</td>
+                  <td>{product.category}</td>
+                  <td>{product.releaseDate.split('T')[0]}</td>
+                  <td>{product.sales} </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
     </div>
   );
 }
