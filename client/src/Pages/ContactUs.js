d7b56b619a0f307491f2a4ab40dcdcf1231c96cf
Elie Bracha <eliebrachapro@gmail.com>
2022-05-30 10:17:12 +0300

    Contact

--- client/src/Pages/ContactUs.js
+++ client/src/Pages/ContactUs.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function ContactUs() {
   return (
     <>
       <h1>Contact Us</h1>
       <ul>
-                                    <li><a href="#">Elie Bracha</a></li>
-                                    <li><a href="#">Jacob Elbaz</a></li>
-                                    <li><a href="#">Adane Adgo</a></li>
-                                    <li><a href="#">Samuel Levy</a></li>
-                                </ul>      </>
-  )
+        <li>
+          <a href="#">Elie Bracha</a>
+        </li>
+        <li>
+          <a href="#">Jacob Elbaz</a>
+        </li>
+        <li>
+          <a href="#">Adane Adgo</a>
+        </li>
+        <li>
+          <a href="#">Samuel Levy</a>
+        </li>
+      </ul>{' '}
+    </>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
