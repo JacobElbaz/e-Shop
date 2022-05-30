3e37331643d39dacf3665ae4df027aff81cc52f9
Elie Bracha <eliebrachapro@gmail.com>
2022-05-30 13:00:53 +0300

    About

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-      <>
+    <div>
       <h1>About</h1>
-      <p>Game Zone is the premiere source and community for both niche and unique games across the globe. We provide fun daily content, memes, and comprehensive coverage of the latest and greatest video games. We are the new enthusiast gaming press, for the digital age.</p>
-      </>
-    
+      <p>
+      Game Zone is the premiere source and community for both niche and unique games across the globe. We provide fun daily content, memes, and comprehensive coverage of the latest and greatest video games. We are the new enthusiast gaming press, for the digital age.
+      </p>
+    </div>
   )
 }
 
